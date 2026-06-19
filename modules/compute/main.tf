b65fc03d95129ed2d448bdfe3f8ff150a7e74d44
205-sys <bunsengsan205@gmail.com>
2026-06-19 07:29:42 +0000

    update nginx default page

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -16,9 +16,7 @@ resource "google_compute_instance" "vm" {
     apt install -y nginx
     systemctl start nginx
     systemctl enable nginx
-
-    echo "<h1>Terraform Deployed Web Server</h1>" > /var/www/html/index.html
-  EOF
+    EOF
 
   network_interface {
     subnetwork = var.subnet_id
