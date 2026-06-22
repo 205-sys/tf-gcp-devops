83a0f37c74c36d4a977039b9a19f13e2372e3581
205-sys <bunsengsan205@gmail.com>
2026-06-22 09:10:27 +0000

    update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,31 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "network" {
+  source      = "../../modules/network"
+  vpc_name    = "dev-vpc"
+  subnet_name = "dev-subnet"
+  cidr        = "10.10.0.0/24"
+  region      = var.region
+}
+
+
+module "compute" {
+  source    = "../../modules/compute"
+  vm_name   = "dev-web-vm"
+  zone      = var.zone
+  subnet_id = module.network.subnet_id
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "office-tf-state-2026-001"
+    prefix = "dev"
+  }
+}
 # Health check
 resource "google_compute_health_check" "http" {
   name = "http-health-check"
